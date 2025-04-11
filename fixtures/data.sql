a3b175182d9ecc6e256101d129586847111bda7d
Daniel Jirik <jirikdaniel@email.cz>
2025-04-11 11:29:57 +0200

    db fix

---
+++ fixtures/data.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.40, for Linux (x86_64)
+--
+-- Host: db    Database: thedatabase
+-- ------------------------------------------------------
+-- Server version	8.0.40
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `wordle`
+--
+
+LOCK TABLES `wordle` WRITE;
+/*!40000 ALTER TABLE `wordle` DISABLE KEYS */;
+INSERT INTO `wordle` (`id`, `word`) VALUES (1,'košař'),(2,'stark'),(3,'roman'),(4,'háček'),(5,'lsblk'),(6,'cisco'),(7,'linux'),(8,'debug'),(9,'robot'),(10,'mysql'),(11,'joooo'),(12,'login'),(13,'class'),(14,'admin'),(15,'tomsn'),(16,'fanos'),(17,'marek'),(18,'tiger'),(19,'niger'),(20,'gypsy'),(21,'route'),(22,'trace'),(23,'mkdir'),(24,'rmdir'),(25,'samba'),(26,'karel'),(27,'pavel'),(28,'quota'),(29,'chmod'),(30,'fstab'),(31,'lemka'),(32,'mýdlo'),(33,'jádra'),(34,'glock'),(35,'bahno'),(36,'bláto'),(37,'obrna'),(38,'klaus'),(39,'milan'),(40,'češka'),(41,'mrdka');
+/*!40000 ALTER TABLE `wordle` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-04-11  9:25:31
